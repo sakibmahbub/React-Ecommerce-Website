b365fb1781af8e96c8ab270fe8ae61e6ce1246f9
sakibmahbub <arafatsakib48@gmail.com>
2023-03-29 02:13:53 +0600

    added total shipping charge dynamically

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -5,15 +5,17 @@ const Cart = (props) => {
   const { cart } = props;
 
   let totalPrice = 0;
+  let totalShipping = 0;
   for (const product of cart) {
     totalPrice = totalPrice + product.price;
+    totalShipping = totalShipping + product.shipping;
   }
   return (
     <div className="cart">
       <h3>Order Summary</h3>
       <h4>Selected Items: {cart.length}</h4>
       <p>Total Price: ${totalPrice}</p>
-      <p>Total Shipping Charge:</p>
+      <p>Total Shipping Charge: ${totalShipping}</p>
       <p>Tax:</p>
       <h4>Grand Total:</h4>
     </div>
