af7e0abd1949d4ed84a77400268db0c2ac07513b
sakibmahbub <arafatsakib48@gmail.com>
2023-03-28 13:30:32 +0600

    displayed img on product component

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,19 +1,23 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import Product from "../Product/Product";
 import "./Shop.css";
 
 const Shop = () => {
+  const [products, setProducts] = useState([]);
   useEffect(() => {
     fetch(
       "https://raw.githubusercontent.com/ProgrammingHero1/ema-john-resources/main/fakeData/products.json"
     )
       .then((res) => res.json())
-      .then((data) => console.log(data));
+      .then((data) => setProducts(data));
   }, []);
 
   return (
     <div className="shop-container">
       <div className="product-container">
-        <h2>Product goes here</h2>
+        {products.map((product) => (
+          <Product key={product.id} product={product}></Product>
+        ))}
       </div>
       <div className="cart-container">
         <h4>Order Summary</h4>
