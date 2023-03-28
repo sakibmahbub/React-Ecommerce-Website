57a6ae20cdec5d496b4bbf143379e7f046b81880
sakibmahbub <arafatsakib48@gmail.com>
2023-03-28 14:34:08 +0600

    displayed name, price, ratings etc on UI

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -3,10 +3,16 @@ import "./Product.css";
 
 const Product = (props) => {
   console.log(props.product);
-  const { name, img, price, seller, quantity } = props.product;
+  const { name, img, price, seller, ratings } = props.product;
   return (
     <div className="product">
       <img src={img} alt="" />
+      <div className="product-info">
+        <h6 className="product-name">{name}</h6>
+        <p>Price: ${price}</p>
+        <p>Manufacturer: {seller}</p>
+        <p>Rating: {ratings}</p>
+      </div>
     </div>
   );
 };
