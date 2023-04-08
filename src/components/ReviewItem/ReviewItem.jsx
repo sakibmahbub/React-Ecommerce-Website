64706e64c7831708a72c4ba93d7e70183fe0510c
sakibmahbub <arafatsakib48@gmail.com>
2023-04-09 01:31:05 +0600

    added style on review items and fontawesome icon

--- src/components/ReviewItem/ReviewItem.jsx
+++ src/components/ReviewItem/ReviewItem.jsx
@@ -1,10 +1,24 @@
 import React from "react";
 import "./ReviewItem.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
 const ReviewItem = ({ product }) => {
   const { id, img, price, name, quantity } = product;
   return (
     <div className="review-item">
       <img src={img} alt="" />
+      <div className="review-details">
+        <p className="review-title">{name}</p>
+        <p>
+          Price: <span className="orange-text">${price}</span>
+        </p>
+        <p>
+          Quantity: <span className="orange-text">{quantity}</span>
+        </p>
+      </div>
+      <button className="btn-delete">
+        <FontAwesomeIcon className="delete-icon" icon={faTrashAlt} />
+      </button>
     </div>
   );
 };
