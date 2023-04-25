a6c4726cc799161ff6d9c0c7c386fb57871f43f3
sakibmahbub <arafatsakib48@gmail.com>
2023-04-26 01:53:58 +0600

    setup auth context

--- src/__steps__.js
+++ src/__steps__.js
@@ -5,3 +5,11 @@
  * 4. Save firebase config and export app
  * 5. Build > Authentication > Get started > Enable sign in method
  */
+
+/**
+ * 1. Create a context provider file
+ * 2. Create a context and set provider
+ * 3. Set the children props
+ * 4. Set context value
+ * 5. Set the provider
+ */
--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,7 @@ import cartProductLoader from "./loaders/cartProductsLoader";
 import Checkout from "./components/Checkout/Checkout";
 import Login from "./components/Login/Login";
 import SignUp from "./components/SignUp/Signup";
+import AuthProvider from "./components/Providers/authProvider";
 const router = createBrowserRouter([
   {
     path: "/",
@@ -45,6 +46,8 @@ const router = createBrowserRouter([
 ]);
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </React.StrictMode>
 );
